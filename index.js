bb0d5f2d89fbef5341eab6e190d2e753a3514f72
Boboinna <andrelle364808@gmail.com>
2026-07-03 00:57:27 +0300

    done

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+require("datejs");
 
+function combineUsers(...args) {
+  let combinedObject = {
+    users: [],
+  };
 
+  // for each of the stuff in the args, combine them with
+  for (let array of args) {
+    combinedObject.users = [...combinedObject.users, ...array]; // spread
+  }
+
+  // add date using dot operator to the object
+  combinedObject.merge_date = new Date.today().toString("M/d/yyyy");
+  return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
